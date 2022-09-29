0e1c4f44712d8913022c613c42fe9ec7ff3964ad
erno <js1239080@gmail.com>
2022-09-30 00:42:47 +0300

    update post/element controllers

--- app/controllers/authors/elements_controller.rb
+++ app/controllers/authors/elements_controller.rb
@@ -1,7 +1,7 @@
 module Authors
   class ElementsController < AuthorsController
     before_action :set_post
-    before_action :set_element, only: [:update, :destroy]
+    before_action :set_element, only: [:update, :destroy, :image]
   
     # POST /elements
     def create
@@ -24,7 +24,7 @@ module Authors
     # DELETE /elements/1
     def destroy
       @element.destroy
-      redirect_to elements_url, notice: 'Element was successfully destroyed.'
+      redirect_to edit_post_path(@element.post)
     end
   
     private
@@ -40,7 +40,7 @@ module Authors
   
       # Only allow a list of trusted parameters through.
       def element_params
-        params.require(:element).permit(:element_type, :content)
+        params.require(:element).permit(:element_type, :content, :image)
       end
   end  
 end
